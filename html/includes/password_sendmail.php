b4396f5bda26e6de2721c43ab978eda692f3092b
bif <bif>
2005-09-06 23:01:17 +0000

    Initial revision

---
+++ html/includes/password_sendmail.php
@@ -0,0 +1,14 @@
+<?
+$mailbody = '
+' . $l['str_email_this_is_an_automatic_email'] . '
+
+' . sprintf($l['str_email_password_recovery_for_account_on_x'],$config->sitename) . '
+
+' . $l['str_email_go_on_followin_page_to_recover'] . '
+
+' . $config->mainurl . 'password.php?id=' . $id . '&amp;language=' . $lang . '
+
+-- 
+' . $config->emailsignature . '
+';
+?>
---
+++ html/includes/user_sendmail.php
@@ -0,0 +1,20 @@
+<?
+$mailbody = '
+' . $l['str_email_this_is_an_automatic_email'] . '
+
+' . sprintf($l['str_email_address_modified_on_x'],$config->sitename) . '
+' . sprintf($l['str_email_this_email_is_sent_to_validate_email_x'],$email) .'
+
+' . $l['str_email_please_follow_this_link'] . '
+' . $config->mainurl . 'validate.php?id=' . $randomid . '&amp;language=' . $lang . '
+
+' . $l['str_email_account_can_not_be_used_without_this_validation'] . '
+
+' . $l['str_email_regards'] . '
+
+-- 
+' . $config->emailsignature . '
+
+
+';
+?>
