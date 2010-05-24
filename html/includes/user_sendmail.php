e1c00e73023a5b1f185a9e57f20679a71ae972f5
bif <bif>
2010-05-24 16:29:17 +0000

    add slashes around user provided email :) drop "account will be locked", this is no longer. remove useless in plain text &amp;

--- html/includes/user_sendmail.php
+++ html/includes/user_sendmail.php
@@ -3,12 +3,10 @@ $mailbody = '
 ' . $l['str_email_this_is_an_automatic_email'] . '
 
 ' . sprintf($l['str_email_address_modified_on_x'],$config->sitename) . '
-' . sprintf($l['str_email_this_email_is_sent_to_validate_email_x'],$email) .'
+' . sprintf($l['str_email_this_email_is_sent_to_validate_email_x'],addslashes($email)) .'
 
 ' . $l['str_email_please_follow_this_link'] . '
-' . $config->mainurl . 'validate.php?id=' . $randomid . '&amp;language=' . $lang . '
-
-' . $l['str_email_account_can_not_be_used_without_this_validation'] . '
+' . $config->mainurl . 'validate.php?id=' . $randomid . '&language=' . $lang . '
 
 ' . $l['str_email_regards'] . '
 
