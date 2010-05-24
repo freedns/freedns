b0caabed34fbf340e6f820b8de6c9fa5b78b32d9
bif <bif>
2010-05-24 15:07:09 +0000

    redirector helper

---
+++ html/empty.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<style type="text/css">
+<!--
+body { margin:50px; background: white; }
+dt {margin-top: 10px; font-style: italic; }
+dd {width: 80%; }
+a {text-decoration: none; }
+.ok {font-size: 120%; }
+-->
+</style>
+</head>
+<body>
+<h1>Ta nazwa nie jest prawid³owa...</h1>
+<p>
+Nazwa <strong><?=$site?></strong> nie jest prawid³owa.
+<br />
+Je¿eli sprowadzi³o Ciê tu klikniêcie w jaki¶ odno¶nik, wróæ do tamtej strony
+i zg³o¶ problem webmasterowi.
+</p>
+<p class="ok">
+W ka¿dym razie zapraszam do us³ugi FreeDNS::SGH
+pod adresem <a href="http://FreeDNS.sgh.waw.pl/"> http://FreeDNS.sgh.waw.pl/</a>
+</p>
+</body>
+</html>
