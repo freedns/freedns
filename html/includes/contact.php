c981c9dd4cc71433acb2bb94a6ef5ab7701f2166
bif <bif>
2010-05-24 16:18:54 +0000

    use sitename from config

--- html/includes/contact.php
+++ html/includes/contact.php
@@ -1,10 +1,9 @@
 <?
 $title = $l['str_contact_title'];
-$content = $l['str_contact_for_bugs_contact_us'] . '<br />
-';
+$content = sprintf($l['str_contact_for_bugs_contact_us'], $config->sitename, $config->emailto, $config->emailto);
 print $html->box('contact',$title,$content);
 
 $title = $l['str_contribute_title'];
-$content = $l['str_contribute_content'];
+$content = sprintf($l['str_contribute_content'], $config->sitename);
 print $html->box('contribute',$title,$content);
 ?>
