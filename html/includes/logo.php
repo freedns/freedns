8869e02e2dab46276db79e2bee82b06a5a421aab
bif <bif>
2010-05-24 15:38:19 +0000

    add sponsors' logos

--- html/includes/logo.php
+++ html/includes/logo.php
@@ -1,4 +1,8 @@
 <?
 print '
-<div id="logo"><a href="http://source.xname.org"><img src="xnamepowered.png" border="0" alt="XName"/></a></div>';
+<div id="logo">
+<a href="http://source.xname.org"><img src="images/xnamepowered.png" alt="XName"></a><br>
+<a href="http://nitronet.pl"><img src="images/nitronet.png" alt="Nitronet"></a><br>
+<a href="http://www.plix.pl"><img src="images/plix.png" alt="PLIX"></a><br>
+</div>';
 ?>
