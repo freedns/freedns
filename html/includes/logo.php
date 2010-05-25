c24e52b177b5883ca5a4e1524ff6a88b57b3b728
bif <bif>
2010-05-25 09:35:28 +0000

    reorder logos

--- html/includes/logo.php
+++ html/includes/logo.php
@@ -1,8 +1,8 @@
 <?
 print '
 <div id="logo">
-<a href="http://source.xname.org"><img src="images/xnamepowered.png" alt="XName"></a><br>
 <a href="http://nitronet.pl"><img src="images/nitronet.png" alt="Nitronet"></a><br>
 <a href="http://www.plix.pl"><img src="images/plix.png" alt="PLIX"></a><br>
+<a href="http://source.xname.org"><img src="images/xnamepowered.png" alt="XName"></a><br>
 </div>';
 ?>
