d69021f0b03b72c6a9b142c2d0980399bdf6387d
bif <bif>
2011-09-17 16:43:23 +0000

    slightly better text

--- html/empty.php
+++ html/empty.php
@@ -13,14 +13,25 @@ a {text-decoration: none; }
 <body>
 <h1>Ta nazwa nie jest prawidłowa...</h1>
 <p>
-Nazwa <strong><?=$site?></strong> nie jest prawidłowa.
+Nazwa <strong><?=$site?></strong> nie jest skonfigurowana na tym serwerze.
 <br>
 Jeżeli sprowadziło Cię tu kliknięcie w jakiś odnośnik, wróć do tamtej strony
 i zgłoś problem webmasterowi.
 </p>
 <p class="ok">
 W każdym razie zapraszam do usługi FreeDNS::42
-pod adresem <a href="http://freedns.42.pl/"> http://freedns.42.pl/</a>
+pod adresem <a href="https://freedns.42.pl/">https://freedns.42.pl/</a>
+</p>
+<h1>This name is not valid...</h1>
+<p>
+Site name <strong><?=$site?></strong> is not configured on this server.
+<br>
+If you came here by clicking on some link, go back and tell webmaster of that site
+about this problem.
+</p>
+<p class="ok">
+In any case I invite you to use FreeDNS::42 service at the
+<a href="https://freedns.42.pl/?language=en">https://freedns.42.pl/</a>
 </p>
 </body>
 </html>
