4e1fbf4a4cf8e3f324637de7c77d411a32ebd898
bif <bif>
2014-10-05 16:19:51 +0000

    add g+ link

--- html/includes/contact.php
+++ html/includes/contact.php
@@ -1,13 +1,15 @@
 <?
 $title = $l['str_contact_title'];
-$content = sprintf($l['str_contact_for_bugs_contact_us'], $config->sitename, '', '‮' . strrev($config->emailto));
+$googleplus = '<p align="center"><a href="https://plus.google.com/112615986755984549471" rel="publisher">Google+</a>';
+$content = sprintf($l['str_contact_for_bugs_contact_us'] . $googleplus,
+                    $config->sitename, '', '‮' . strrev($config->emailto));
 print $html->box('contact',$title,$content);
 
 $title = $l['str_sponsor_title'];
 $content = '<p>
 <a href="http://nitronet.pl"><img src="images/nitronet.png" alt="Nitronet"></a><br>
 <a href="http://www.plix.pl"><img src="images/plix.png" alt="PLIX"></a><br>
-<a href="'.$link.'&what=thanks">' . $l['str_thanks'] . '</a>
+<a href="/index.php'.$link.'&what=thanks">' . $l['str_thanks'] . '</a>
 </p>';
 print $html->box('sponsor', $title, $content);
 
