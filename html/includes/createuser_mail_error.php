53bd2cc185df6f47aa279dfd06c2f1ec0208205e
Piotr KUCHARSKI <freedns@42.pl>
2014-10-24 18:28:12 +0200

    space cleanups. move two tiny include files used only in createuser.php directly into it.

--- html/includes/createuser_mail_error.php
+++
@@ -1,8 +0,0 @@
-<?
-$content .= $l['str_email_error_error_occured'] . '
-' . sprintf($l['str_email_error_please_verify_address_x'],$email) .'
-' . sprintf($l['str_in_doubt_you_can_contact_us_at_x'],
-'<a href="mailto:' . $config->contactemail . '" 
-class="linkcolor">' . $config->contactemail . '</a>') . '
-';
-?>
