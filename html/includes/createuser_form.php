27fa0a73156b65bcc7d4ef177dd6f1e686cf1c53
Piotr KUCHARSKI <freedns@42.pl>
2014-10-24 19:28:30 +0200

    spacing and sprintf cleanup

--- html/includes/createuser_form.php
+++ html/includes/createuser_form.php
@@ -1,107 +1,87 @@
-<?
-      $content .= '
-<form action="' . $PHP_SELF . '" method="post">
-      <table id="createusertable">
-      <tr><td align="right">
-      ' . $l['str_login'] . ': </td><td><input type="text" name="loginnew"
-      value="'.$loginnew.'">
-      </td></tr>
-      <tr><td align=right>
-      ' . $l['str_your_valid_email'] . ': </td>
-      <td><input type="text" name="email" value="'.$email.'">
-      </td></tr>
-      <tr><td align="right">
-      ' . $l['str_new_password'] . ': </td>
-      <td><input type="password" name="passwordnew">
-      </td></tr>
-      <tr><td align="right">
-      ' . $l['str_confirm_password'] . ': </td>
-      <td><input type="password" name="confirmpasswordnew">
-      </td></tr>      ';
+<?php
+  # basic info
+  $content .= sprintf('<form action="%s" method="post">', $_SERVER['PHP_SELF']);
+  $content .= '<table id="createusertable"><tr>';
+  $content .= sprintf('<td align="right">%s:</td>', $l['str_login']);
+  $content .= sprintf('<td><input type="text" name="loginnew" value="%s"></td>', $loginnew);
+  $content .= '</tr><tr>';
+  $content .= sprintf('<td align=right>%s</td>', $l['str_your_valid_email']);
+  $content .= sprintf('<td><input type="text" name="email" value="%s"></td>', $email);
+  $content .= '</tr><tr>';
+  $content .= sprintf('<td align="right">%s:</td>', $l['str_new_password']);
+  $content .= '<td><input type="password" name="passwordnew"></td>';
+  $content .= '</tr><tr>';
+  $content .= sprintf('<td align="right">%s:</td>', $l['str_confirm_password']);
+  $content .= '<td><input type="password" name="confirmpasswordnew"></td>';
+  $content .= '</tr>';
 
-    if($config->advancedinterface){
-      $content .= '<tr><td align="right">' . $l['str_advanced_interface']  . 
-      '<br>(' . $l['str_advanced_interface_details'] . ')</td>
-      <td><input type=checkbox name="advanced"';
-      if($user->advanced || $_REQUEST['advanced']){
-        $content .= ' checked';
-      }
-      $content .='></td></tr>
-      ';
-    }
-    if($config->ipv6interface){
-      $content .= '<tr><td align="right">' . 
-      $l['str_ipv6_interface'] . '<br>(' . 
-      $l['str_ipv6_interface_details'] . ')</td>
-      <td><input type=checkbox name="ipv6"';
-      if($user->ipv6 || $_REQUEST['ipv6']){
-        $content .= ' checked';
-      }
-      $content .='></td></tr>
-      ';
-    }
-    if($config->txtrecords){
-      $content .= '<tr><td align="right">' . 
-      $l['str_txt_records'] . '<br>(' . 
-      $l['str_txt_records_details'] . ')</td>
-      <td><input type=checkbox name="txtrecords"';
-      if($user->txtrecords || $_REQUEST['txtrecords']){
-        $content .= ' checked';
-      }
-      $content .='></td></tr>
-      ';
-    }
-    if($config->srvrecords){
-      $content .= '<tr><td align="right">' . 
-      $l['str_srv_records'] . '<br>(' . 
-      $l['str_srv_records_details'] . ')</td>
-      <td><input type=checkbox name="srvrecords"';
-      if($user->srvrecords || $_REQUEST['srvrecords']){
-        $content .= ' checked';
-      }
-      $content .='></td></tr>
-      ';
-    }
-    
-    if ($user->authenticated)
-      $nbrows = $user->nbrows > $nbrows ? $user->nbrows : $nbrows;
-    else
-      $nbrows = $_REQUEST['nbrows'] > $nbrows ? $_REQUEST['nbrows'] : $nbrows;
-    if (empty($nbrows)) $nbrows = 3;
-    if ($nbrows>16) $nbrows=16;
-    $content .= '<tr><td align="right">' . 
-    $l['str_number_of_rows_per_record'] . ':</td>
-';
-    $content .= '<td><input type=text name="nbrows" value="' . $nbrows . '" size="3"></td></tr>
-';
-      
-    $content .= '<tr><td align="right">' . 
-          $l['str_language']  . ':</td>
-          <td><select name="newlang">';
-    // select current available langs
-    $list = GetDirList('includes/strings');
-    reset($list);
-    while($newlang = array_shift($list)){
-      if(!strcmp($newlang,$lang)){
-        $content .= '<option value="' . $lang . '" selected>' . $lang . '</option>';
-      }else{
-        $content .= '<option value="' . $newlang . '">' . $newlang . '</option>';      
-      }
-    }
-    
-    $content .= '</select></td></tr>';
+  # advanced parameters checkboxes
+  if ($config->advancedinterface) {
+    $content .= sprintf('<tr><td align="right">%s<br>(%s)</td>',
+        $l['str_advanced_interface'], $l['str_advanced_interface_details']);
+    $content .= sprintf('<td><input type=checkbox name="advanced"%s></td></tr>',
+      ($user->advanced || $_REQUEST['advanced']) ? ' checked' : '');
+  }
+  if ($config->ipv6interface) {
+    $content .= sprintf('<tr><td align="right">%s<br>(%s)</td>',
+        $l['str_ipv6_interface'], $l['str_ipv6_interface_details']);
+    $content .= sprintf('<td><input type=checkbox name="ipv6"%s></td></tr>',
+        ($user->ipv6 || $_REQUEST['ipv6']) ? ' checked' : '');
+  }
+  if ($config->txtrecords) {
+    $content .= sprintf('<tr><td align="right">%s<br>(%s)</td>',
+        $l['str_txt_records'], $l['str_txt_records_details']);
+    $content .= sprintf('<td><input type=checkbox name="txtrecords"%s></td></tr>',
+        ($user->txtrecords || $_REQUEST['txtrecords']) ? ' checked' : '');
+  }
+  if ($config->srvrecords) {
+    $content .= sprintf('<tr><td align="right">%s<br>(%s)</td>',
+        $l['str_srv_records'], $l['str_srv_records_details']);
+    $content .= sprintf('<td><input type=checkbox name="srvrecords"%s></td></tr>',
+        ($user->srvrecords || $_REQUEST['srvrecords']) ? ' checked' : '');
+  }
 
-      $content .= '
-      <tr><td colspan="2">' . 
-      sprintf($l['str_i_have_read_and_i_understand_discl_available_at_x'],
-      '<br>
-<a target=_blank href="disclaimer.php?language=' . $lang . '" 
-onclick="window.open(\'disclaimer.php?language=' . $lang . '\',\'M\',\'toolbar=no,location=no,directories=no,status=no,alwaysraised=yes,dependant=yes,menubar=no,scrollbars=yes,resizable=yes,width=640,height=480\');
-return false">' . $config->mainurl . 'disclaimer.php</a>') . '
-       <input type="checkbox" name="ihaveread" value="1"></td></tr>
-      <tr><td colspan="2" align="center">
-      <input type="submit" class="submit" value="' . $l['str_create_my_user_button'] . '"></td></tr>
-      </table>
-</form>
-';
+  # number-of-entries row
+  if (empty($nbrows))
+    $nbrows = $config->defaultnbrows;
+  if ($user->authenticated) {
+    if ($user->nbrows > $nbrows)
+      $nbrows = $user->nbrows;
+  } else {
+    if (!empty($_REQUEST['nbrows']) && $_REQUEST['nbrows'] > $nbrows)
+      $nbrows = $_REQUEST['nbrows'];
+  }
+  if ($nbrows > 16)
+    $nbrows = 16;
+  $content .= sprintf('<tr><td align="right">%s:</td>', $l['str_number_of_rows_per_record']);
+  $content .= sprintf('<td><input type=text name="nbrows" value="%s" size="3"></td></tr>', $nbrows);
+
+  # language row
+  $content .= sprintf('<tr><td align="right">%s:</td>', $l['str_language']);
+  $content .= '<td><select name="newlang">';
+  // select current available langs
+  $list = GetDirList('includes/strings');
+  reset($list);
+  while ($newlang = array_shift($list)) {
+    $content .= sprintf('<option value="%s"%s>%s</option>',
+        $newlang, ($newlang === $lang) ? ' selected' : '', $newlang);
+  }
+  $content .= '</select></td></tr>';
+
+  # disclaimer row
+  $content .= '<tr><td width="65%" align="right">';
+  $_url = sprintf('disclaimer.php?language=%s', $lang);
+  $_url2 = sprintf('%sdisclaimer.php', $config->mainurl);
+  $_winopt = sprintf("'%s','M','toolbar=no,location=no,directories=no," .
+      "status=no,alwaysraised=yes,dependant=yes,menubar=no,scrollbars=yes," .
+      "resizable=yes,width=640,height=480'", $_url);
+  $_disclaimer = sprintf('<a target=_blank href="%s" onclick="window.open(%s)">%s</a>',
+      $_url, $_winopt, $_url2);
+  $content .= sprintf($l['str_i_have_read_and_i_understand_discl_available_at_x'], $_disclaimer);
+  $content .= '</td>';
+  $content .= '<td><input type="checkbox" name="ihaveread" value="1"></td></tr>';
+  $content .= '<tr><td colspan="2" align="center">';
+  $content .= sprintf('<input type="submit" class="submit" value="%s"></td></tr>',
+      $l['str_create_my_user_button']);
+  $content .= '</table></form>';
 ?>
